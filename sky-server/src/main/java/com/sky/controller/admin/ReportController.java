b6e7158e1792a62fe923fcacdb3dbfbfdf2d5d6d
tanzheng365 <tanzheng365@163.com>
2023-10-14 18:25:31 +0800

    导出运营数据报表代码开发

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -22,6 +22,8 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 /**
@@ -100,5 +102,15 @@ public class ReportController {
         return Result.success(reportService.getSalesTop10(begin,end));
     }
 
+
+    /**
+     * 导出运营数据报表
+     * @param response
+     */
+    @GetMapping("/export")
+    @ApiOperation("导出运营数据报表")
+    public void export(HttpServletResponse response){
+        reportService.exportBusinessData(response);
+    }
 
 }
--- sky-server/src/main/java/com/sky/service/ReportService.java
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -15,6 +15,7 @@ import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 public interface ReportService {
@@ -54,6 +55,9 @@ public interface ReportService {
     SalesTop10ReportVO getSalesTop10(LocalDate begin, LocalDate end);
 
 
-
-
+    /**
+     * 导出近30天的运营数据报表
+     * @param response
+     **/
+    void exportBusinessData(HttpServletResponse response);
 }
